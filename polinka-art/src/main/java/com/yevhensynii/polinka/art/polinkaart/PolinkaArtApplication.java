68aa6174b9faab7599ce7cd142c95b4d6ae66dbc
EuhenySiniy <justbusinessgroupsellers@gmail.com>
2023-01-29 21:07:28 +0100

    Start point

---
+++ polinka-art/src/main/java/com/yevhensynii/polinka/art/polinkaart/PolinkaArtApplication.java
@@ -0,0 +1,13 @@
+package com.yevhensynii.polinka.art.polinkaart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolinkaArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolinkaArtApplication.class, args);
+	}
+
+}
---
+++ polinka-art/src/test/java/com/yevhensynii/polinka/art/polinkaart/PolinkaArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yevhensynii.polinka.art.polinkaart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolinkaArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
